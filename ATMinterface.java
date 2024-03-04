5c1f1864d5968b538b2a5a1a47c3e02007ed1558
gundemeda-manasa <57957891+gundemeda-manasa@users.noreply.github.com>
2024-03-04 23:17:19 +0530

    Add files via upload

---
+++ ATMinterface.java
@@ -0,0 +1,10 @@
+package ATM;
+
+public interface ATMinterface {
+	
+	    public void viewBalance();
+	    public void withdrawAmount(double withdrawAmount);
+	    public void depositAmount(double depositAmount );
+	    public  void viewMiniStatement();
+
+}
